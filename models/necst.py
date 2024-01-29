e8e8ddcd754be259a7c9263c7ec0474f91d14fcb
Daniel Nguyen <mduongbkhn@gmail.com>
2024-01-29 12:52:34 +0700

    model

--- models/necst.py
+++ models/necst.py
@@ -1,8 +1,90 @@
+import os
+import pandas
+import matplotlib.pyplot as plt
+import seaborn
+from tqdm import tqdm
+import numpy as np
+
 import torch
-import torch.nn as nn
+from torch import nn
 
 
-class NECST(nn.Module):
-    def __init__(self):
-        pass
+class CNN_CIFAR_NoVAE(nn.Module):
+    def __init__(self, args, in_channel, class_num):
+        super(CNN_CIFAR_NoVAE, self).__init__()
 
+        self.in_channel = in_channel
+        self.class_num = class_num
+        self.inv_cdim = args.inv_cdim    # int(32)  # inv_cdim
+        self.var_cdim = args.var_cdim    # int(32)  # var_cdim
+        self.ib_cdim = self.inv_cdim + self.var_cdim
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(self.in_channel, 32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=self.var_cdim, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(self.var_cdim),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(self.ib_cdim, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.ConvTranspose2d(32, in_channel, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.SELU(),
+        )
+
+    def forward(self, x):
+        enc = self.encoder(x)
+        rec = self.decoder(enc)
+        return rec
+
+    def get_latent(self, x):
+        enc = self.encoder(x)
+        return enc
+
+    def get_semcom_recon(self, x, n_var, ukie_flag, device):
+        enc = self.encoder(x)
+
+        # generate Gaussian propagating noise
+        noise = torch.normal(mean=torch.zeros(enc.size()),
+                             std=torch.ones(enc.size()) * n_var).to(device)
+
+        # feedforward latents
+        if not ukie_flag:
+            noise_i = torch.normal(mean=torch.zeros(enc.size()),
+                                   std=torch.ones(enc.size()) * n_var).to(device)
+            inv = enc + noise_i
+        else:
+            inv = enc
+        var = enc + noise  # simulate a noise by physical channels
+
+        rec = self.decoder(var)
+        return rec
+
+    def get_latent_size(self, x):
+        enc = self.encoder(x)
+        return enc.size()
--- train.py
+++ train.py
@@ -1,3 +1,25 @@
+import os
+import argparse
+from tqdm import tqdm
+import numpy as np
+
+from dataset import *
+from utils.log import Log, Model_Info, interpolate, inference
+from utils.logging import Logging
+from datetime import datetime
+from glob import glob
+import shutil
+
+import torch
+from torch import nn
+from torch.optim import Adam
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import Dataset, DataLoader
+# from torchvision.datasets import EMNIST
+from torchvision import transforms
+from models import *
+
+
 def train_necst(args: argparse):
     # Folder Setup
     runs_dir = os.getcwd() + "/runs"
@@ -72,15 +94,63 @@ def train_necst(args: argparse):
         {'params': model.inv.parameters()},
         {'params': model.classifier.parameters()}], lr=args.lr)
 
-    criterion = InvLoss(
-        rec_loss=args.recl,
-        cls_loss=args.clsl,
-        dis_loss=args.disl
-    )
+    criterion = nn.MSELoss()
 
     # Training
     old_loss_value = 1e26
     old_acc_value = 0
+    tr_total_loss = 0
+    temp_loss = 0
+    psnr_train = 0
+    model.train()
+    for _ in range(args.overall_e):
+        for x, y in tqdm(train_dl):
+            x, y = x.to(device), y.to(device)
+            logits, rec, inv, var, mu, logvar = model(x)
+
+            ov_loss_dict = criterion(args, logits, rec, inv, var, x, y, mu, logvar)
+
+            ukie_optimizer.zero_grad()
+            ov_loss_dict["total_loss"].backward()
+            ukie_optimizer.step()
+
+            log_interface(key=f"train/loss/total", value=ov_loss_dict["total_loss"].item())
+            log_interface(key=f"train/loss/rec", value=ov_loss_dict["rec_loss"].item())
+            log_interface(key=f"train/loss/kld", value=ov_loss_dict["kld_loss"].item())
+            log_interface(key=f"train/loss/inv", value=ov_loss_dict["inv_loss"].item())
+            log_interface(key=f"train/loss/var", value=ov_loss_dict["var_loss"].item())
+
+            logits, rec, inv, var, mu, logvar = model(x)
+            ov_loss_dict["accuracy"] = logits.max(1)[1].eq(y).sum() / y.size(0)
+            temp_loss += ov_loss_dict["accuracy"]
+            tr_total_loss += ov_loss_dict['rec_loss'].item()
+            psnr_train += (20 * torch.log10(torch.max(x) / torch.sqrt(ov_loss_dict['rec_loss']))).item()
+
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for test_imgs, test_labels in tqdm(test_dl):
+            test_imgs = test_imgs.to(device, non_blocking=True)
+            test_labels = test_labels.to(device, non_blocking=True)
+            test_logits, test_rec, test_inv, test_var, test_mu, test_logvar = model(test_imgs)
+            test_loss_dict = criterion(args, test_logits, test_rec,
+                                       test_inv, test_var,
+                                       test_imgs, test_labels,
+                                       test_mu, test_logvar)
+
+            _, predicted = test_logits.max(1)
+            total += test_labels.size(0)
+            correct += predicted.eq(test_labels).sum().item()
+
+            test_loss_dict["accuracy"] = test_logits.max(1)[1].eq(test_labels).sum() / test_labels.size(0)
+
+    test_acc = correct * 100 / total
+    if args.verbose:
+        print(f"Test Accuracy: {test_acc}")
+        print(" - ".join([f"{key}: {round(test_loss_dict[key].item(), 5)}" for key in test_loss_dict]))
+        print(" - ".join(
+            [f"{key}: {round(test_loss_dict[key].item() * coeff_dict[key], 5)}" for key in test_loss_dict]))
 
 
 def train_djsccf(args):
