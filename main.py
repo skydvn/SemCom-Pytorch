96e5e9690879854f4d4a489d04b27b1a7162869e
skydvn <mduongbkhn@gmail.com>
2025-03-07 22:42:30 +0900

    m

--- main.py
+++ main.py
@@ -19,56 +19,54 @@ if __name__ == "__main__":
     parser.add_argument('--ratio_list', default=['1/6', '1/12'], nargs='+', help='ratio_list')
     parser.add_argument('--channel', default='AWGN', type=str,
                         choices=['AWGN', 'Rayleigh'], help='channel')
-    # parser.add_argument("--recl", type=str, default='mse',
-    #                     help="Reconstruction Loss")
-    # parser.add_argument("--clsl", type=str, default='ce',
-    #                     help="Classification Loss")
-    # parser.add_argument("--disl", type=str, default='kl',
-    #                     help="Invariance and Variance Loss")
-    # parser.add_argument("--lr", type=float, default=0.01,
-    # #                     help="Inner learning Rate")
-    #
-    # # Loss Setting
-    # parser.add_argument("--cls-coeff", type=float, default=0.5,
-    #                     help="Coefficient for Classification Loss")
-    # parser.add_argument("--rec-coeff", type=float, default=1,
-    #                     help="Coefficient for Reconstruction Loss")
-    # parser.add_argument("--inv-coeff", type=float, default=0.2,
-    #                     help="Coefficient for Invariant Loss")
-    # parser.add_argument("--var-coeff", type=float, default=0.2,
-    #                     help="Coefficient for Variant Loss")
-    #
-    # # Model Setting
-    # parser.add_argument("--inv-cdim", type=int, default=32,
-    #                     help="Channel dimension for invariant features")
-    # parser.add_argument("--var-cdim", type=int, default=32,
-    #                     help="Channel dimension for variant features")
-    #
-    # # VAE Setting
-    # parser.add_argument("--vae", action="store_true",
-    #                     help="vae switch")
-    # parser.add_argument("--kld-coeff", type=float, default=0.00025,
-    #                     help="VAE Weight Coefficient")
-    #
-    # # Meta Setting
+    parser.add_argument("--recl", type=str, default='mse',
+                        help="Reconstruction Loss")
+    parser.add_argument("--clsl", type=str, default='ce',
+                        help="Classification Loss")
+    parser.add_argument("--disl", type=str, default='kl',
+                        help="Invariance and Variance Loss")
+    parser.add_argument("--lr", type=float, default=0.01,
+    #                     help="Inner learning Rate")
+
+    # Loss Setting
+    parser.add_argument("--cls-coeff", type=float, default=0.5,
+                        help="Coefficient for Classification Loss")
+    parser.add_argument("--rec-coeff", type=float, default=1,
+                        help="Coefficient for Reconstruction Loss")
+    parser.add_argument("--inv-coeff", type=float, default=0.2,
+                        help="Coefficient for Invariant Loss")
+    parser.add_argument("--var-coeff", type=float, default=0.2,
+                        help="Coefficient for Variant Loss")
+
+    # Model Setting
+    parser.add_argument("--inv-cdim", type=int, default=32,
+                        help="Channel dimension for invariant features")
+    parser.add_argument("--var-cdim", type=int, default=32,
+                        help="Channel dimension for variant features")
+
+    # VAE Setting
+    parser.add_argument("--vae", action="store_true",
+                        help="vae switch")
+    parser.add_argument("--kld-coeff", type=float, default=0.00025,
+                        help="VAE Weight Coefficient")
+
+    # Meta Setting
     parser.add_argument("--bs", type=int, default=128,
                         help="#batch size")
-    # parser.add_argument("--tsbs", type=int, default=1024,
-    #                     help="test batch size")
     parser.add_argument("--wk", type=int, default=os.cpu_count(),
                         help="#number of workers")
-    # parser.add_argument("--out-e", type=int, default=1,
-    #                     help="#number of epochs")
-    # parser.add_argument("--overall-e", type=int, default=1,
-    #                     help="#number of epochs for overall training")
-    # parser.add_argument("--irep-e", type=int, default=1,
-    #                     help="#number of epochs for invariant representation learning")
-    # parser.add_argument("--dv", type=int, default=0,
-    #                     help="Index of GPU")
-    # parser.add_argument("--model-log", type=bool, default=True,
-    #                     help="save model log")
-    # parser.add_argument("--operator", type=str, default='window',
-    #                     help="Operator for Pycharm")
+    parser.add_argument("--out-e", type=int, default=1,
+                        help="#number of epochs")
+    parser.add_argument("--overall-e", type=int, default=1,
+                        help="#number of epochs for overall training")
+    parser.add_argument("--irep-e", type=int, default=1,
+                        help="#number of epochs for invariant representation learning")
+    parser.add_argument("--dv", type=int, default=0,
+                        help="Index of GPU")
+    parser.add_argument("--model-log", type=bool, default=True,
+                        help="save model log")
+    parser.add_argument("--operator", type=str, default='window',
+                        help="Operator for Pycharm")
 
     # LOGGING
     parser.add_argument('--wandb', action='store_true',
