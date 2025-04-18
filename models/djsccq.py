130f431f64e0b3b6ef6d7f8ff94e19844860ef8d
skydvn <mduongbkhn@gmail.com>
2025-04-18 23:07:54 +0900

    m

---
+++ models/djsccq.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+
+from channels.channel_base import Channel
+from models.model_base import BaseModel
+
+
+class DJSCCQ_CIFAR(BaseModel):
+    def __init__(self, args, in_channel, class_num):
+        super(DJSCCQ_CIFAR, self).__init__(args, in_channel, class_num)
+
+        self.channel_type = "AWGN"
+        self.base_snr = None
+        self.channel = Channel(channel_type="AWGN", snr = args.base_snr)
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(self.in_channel, 32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=self.var_cdim, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(self.var_cdim, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.ConvTranspose2d(32, in_channel, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.SELU(),
+        )
+
+    def forward(self, x):
+        z = self.encoder(x)
+        z = self.normalize_layer(z)
+        z_hat = self.channel(z)
+        x_hat = self.decoder(z_hat)
+        return x_hat, z
+
+    def get_latent(self, x):
+        enc = self.encoder(x)
+        enc = self.normalize_layer(enc)
+        return enc
+
+    def get_train_recon(self, x, base_snr):
+        z = self.encoder(x)
+        z = self.normalize_layer(z)
+        # print("Debug")
+        z = self.channel(z)
+
+        x_hat = self.decoder(z)
+        return x_hat
+
+    def get_latent_size(self, x):
+        enc = self.encoder(x)
+        enc = self.normalize_layer(enc)
+        return enc.size()
+
+    def normalize_layer(self, z):
+        k = torch.tensor(1.0).to(self.device)  # torch.prod(torch.tensor(z.size()[1:], dtype=torch.float32))
+        # Square root of k and P
+        sqrt1 = torch.sqrt(k * self.P)
+
+        # Conjugate Transpose of z
+        # if torch.is_complex(z):
+        #     zT = torch.conj(z).permute(0, 1, 3, 2)
+        # else:
+        #     zT = z.permute(0, 1, 3, 2)
+        # Multiply z and zT = sqrt2
+        sqrt2 = torch.sqrt(z*z + self.e)
+        div = z / sqrt2
+        z_out = div * sqrt1
+
+        return z_out
+
+    def change_channel(self, channel_type='AWGN', snr=None):
+        if snr is None:
+            self.channel = None
+        else:
+            self.channel = Channel(channel_type, snr)
+
+    def get_channel(self):
+        if hasattr(self, 'channel') and self.channel is not None:
+            return self.channel.get_channel()
+        return None
---
+++ train/train_djsccQ.py
@@ -0,0 +1,88 @@
+from tqdm import tqdm
+import numpy as np
+import os
+import glob
+
+import torch
+from torch import nn
+from torch.optim import Adam
+
+from train.train_base import BaseTrainer
+from models.djsccq import *
+
+
+class DJSCCQTrainer(BaseTrainer):
+    def __init__(self, args):
+        super().__init__(args)
+
+        self.model = DJSCCQ_CIFAR(self.args, self.in_channel, self.class_num).to(self.device)
+        self.optimizer = Adam(self.model.parameters(), lr=self.args.lr)
+        self.criterion = DJSCCQLoss()
+        self.base_snr = args.base_snr
+
+    def train(self):
+
+        for epoch in range(self.args.out_e):
+            print(f"Epoch {epoch}")
+            epoch_train_loss = 0
+            epoch_val_loss = 0
+
+            self.model.train()
+            for x, y in tqdm(self.train_dl):
+                x, y = x.to(self.device), y.to(self.device)
+                rec, z = self.model(x)
+                """ 
+                    Measure p_c
+                    - Params: z: (16x3x3) 
+                    - c_j (QAM-4): (4x1): 0001 0010
+                    -> Use Eq. (18) 
+                    
+                """
+                p_c = 0 + 0 + 0 + 0
+                loss = self.criterion.forward(self.args, x, rec, p_c)
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                epoch_train_loss += loss.detach().item()
+            epoch_train_loss /= (len(self.train_dl))
+            self.writer.add_scalar('train/_loss', epoch_train_loss, epoch)
+
+            self.model.eval()
+            with torch.no_grad():
+                for test_imgs, test_labels in tqdm(self.test_dl):
+                    test_imgs, test_labels = test_imgs.to(self.device), test_labels.to(self.device)
+                    test_rec = self.model.get_train_recon(test_imgs, self.base_snr)
+                    loss = self.criterion.forward(self.args, test_imgs, test_rec)
+                    epoch_val_loss += loss.detach().item()
+                epoch_val_loss /= (len(self.test_dl))
+                self.writer.add_scalar('val/_loss', epoch_val_loss, epoch)
+
+            # Saving checkpoint
+            self.save_model(epoch=epoch, model=self.model)
+
+        self.writer.close()
+        self.save_config()
+
+
+class DJSCCQLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.rec_loss = nn.MSELoss()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu", index=0)
+
+    def forward(self, args, rec, img, p_c):
+        """
+        :param args:
+        :param rec:
+        :param img:
+        :param P(C): Approximated from Eq. (18)
+        :param U(C): If QAM-4 -> has 4 nodes -> U_c = 1/4
+        :return: Loss as in Eq. (21)
+        """
+        rec_loss = self.rec_loss(rec, img)
+        total_loss = args.rec_coeff * rec_loss
+
+        return total_loss
